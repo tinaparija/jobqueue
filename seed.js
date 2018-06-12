130faa14bcb273a6da20507de011413432fd2948
tinaparija <tinaparija@gmail.com>
2018-06-12 10:46:48 -0700

    comments

--- seed.js
+++ seed.js
@@ -1,17 +1,1 @@
-// This file allows us to seed our application with data
-// simply run: `node seed.js` from the root of this project folder.
-
 var db = require('./models');
-
-db.Mood.remove({}, function(err, moods){
-	db.Song.remove({}, function (err, songs) {
-		moods_list.forEach(function(moods) {
-			db.Song.create(moods.songs, function (err, savedSongs) {
-				moods.songs = savedSongs;
-				db.Mood.create(moods, function (err, savedList){
-					console.log("saved list", savedList.name)
-				});
-			});
-		});
-	});
-});
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
-//require express in our app
 var express = require('express'),
   bodyParser = require('body-parser'),
   db = require('./models');
 
-// generate a new express app and call it 'app'
 var app = express();
 
 // require axios 
 var axios = require('axios')
 
-// serve static files in public
 app.use(express.static('public'));
 
-// body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
